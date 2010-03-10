be865422e3aa3b4e297665c259f31f3f3ef1b268
Matt Jackson <mw.jackson@hotmail.com>
2010-03-10 21:22:15 +0000

    - initial tests

--- src/fs-500-tests/DeckTests.fs
+++ src/fs-500-tests/DeckTests.fs
@@ -11,10 +11,27 @@ type Suit =
 type Card() =
     member x.Dummy = ""
 
+type Deck() =
+    member x.Deal(numberOfCards) = 
+        List.init<Card> numberOfCards (fun i -> new Card())
+
 [<TestFixture>]
-type DeckTests = 
+type DeckTests() = 
+
+    [<Test>]
+    member test.dealing_should_return_a_list_of_cards() = 
+        let deck = new Deck()
+        let card = deck.Deal(1)
+        Assert.That(card, Is.TypeOf(typedefof<List<Card>>))
 
     [<Test>]
     member test.dealing_one_card_should_return_a_card() = 
-        let card = ""
-        Assert.That(card, Is.TypeOf(typeof<Card>))
+        let deck = new Deck()
+        let cards = deck.Deal(1)
+        Assert.That(cards.Length, Is.EqualTo(1))
+
+    [<Test>]
+    member test.dealing_two_cards_should_return_two_cards() = 
+        let deck = new Deck()
+        let cards = deck.Deal(2)
+        Assert.That(cards.Length, Is.EqualTo(2))
