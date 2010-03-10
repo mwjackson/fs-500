ef8a0e69eb7bfd3ae441c39fcdc0533cf0357cc1
Matt Jackson <mw.jackson@hotmail.com>
2010-03-10 20:50:12 +0000

    - initial commit

---
+++ src/fs-500-tests/DeckTests.fs
@@ -0,0 +1,20 @@
+﻿namespace Fivehundred
+
+open NUnit.Framework
+
+type Suit =
+    | Spades
+    | Clubs
+    | Diamonds
+    | Hearts
+
+type Card() =
+    member x.Dummy = ""
+
+[<TestFixture>]
+type DeckTests = 
+
+    [<Test>]
+    member test.dealing_one_card_should_return_a_card() = 
+        let card = ""
+        Assert.That(card, Is.TypeOf(typeof<Card>))
