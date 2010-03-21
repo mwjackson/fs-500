de89794150a4b8548682271ecb38277efcdfc45f
Matt Jackson <mw.jackson@hotmail.com>
2010-03-21 14:25:23 +0000

    - bids correctly sorted

--- src/fs-500-tests/BiddingTests.fs
+++ src/fs-500-tests/BiddingTests.fs
@@ -32,7 +32,7 @@ type BiddingTests() =
 type BidSortTests() =
    
     [<Test>]
-    member test.a_bid_of_higher_value_should_be_ordered_first() =
+    member test.a_bid_of_higher_value_should_be_higher_than_a_bid_of_lesser_value() =
         let bids = [ Bid(6, Hearts); Bid(8, Hearts); Bid(7, Hearts) ]
         let bidSort = new BidSort()
         let result = bids |> List.sortWith (fun a b -> bidSort.Sort(a, b))
@@ -41,7 +41,7 @@ type BidSortTests() =
         Assert.That(result, Is.EqualTo(expected))
 
     [<Test>]
-    member test.a_bid_of_higher_value_should_be_ordered_before_a_notrumps_of_lesser_value() =
+    member test.a_bid_of_higher_value_should_be_higher_then_a_notrumps_of_lesser_value() =
         let bids = [ NoTrumps(6); Bid(8, Hearts); Bid(7, Hearts) ]
         let bidSort = new BidSort()
         let result = bids |> List.sortWith (fun a b -> bidSort.Sort(a, b))
@@ -50,10 +50,38 @@ type BidSortTests() =
         Assert.That(result, Is.EqualTo(expected))
 
     [<Test>]
-    member test.a_notrumps_of_same_value_should_be_ordered_before_a_bid_of_same_value() =
+    member test.a_notrumps_of_same_value_should_be_higher_then_a_bid_of_same_value() =
         let bids = [ Bid(6, Hearts); Bid(6, Diamonds); NoTrumps(6);]
         let bidSort = new BidSort()
         let result = bids |> List.sortWith (fun a b -> bidSort.Sort(a, b))
         
         let expected = [ NoTrumps(6); Bid(6, Hearts); Bid(6, Diamonds) ]
+        Assert.That(result, Is.EqualTo(expected))
+
+    [<Test>]
+    member test.a_hearts_of_same_value_should_be_higher_than_a_diamond() =
+        let bids = [ Bid(6, Diamonds); Bid(6, Hearts); ]
+        let bidSort = new BidSort()
+        let result = bids |> List.sortWith (fun a b -> bidSort.Sort(a, b))
+        
+        let expected = [ Bid(6, Hearts); Bid(6, Diamonds) ]
+        Assert.That(result, Is.EqualTo(expected))
+
+    [<Test>]
+    member test.a_diamonds_of_same_value_should_be_higher_than_a_clubs() =
+        let bids = [ Bid(6, Clubs); Bid(6, Diamonds); ]
+        let bidSort = new BidSort()
+        let result = bids |> List.sortWith (fun a b -> bidSort.Sort(a, b))
+        
+        let expected = [ Bid(6, Diamonds); Bid(6, Clubs) ]
+        Assert.That(result, Is.EqualTo(expected))
+
+        
+    [<Test>]
+    member test.a_clubs_of_same_value_should_be_higher_than_a_spades() =
+        let bids = [ Bid(6, Spades); Bid(6, Clubs); ]
+        let bidSort = new BidSort()
+        let result = bids |> List.sortWith (fun a b -> bidSort.Sort(a, b))
+        
+        let expected = [ Bid(6, Clubs); Bid(6, Spades) ]
         Assert.That(result, Is.EqualTo(expected))
--- src/fs-500/Bidding.fs
+++ src/fs-500/Bidding.fs
@@ -1,17 +1,30 @@
 ﻿namespace Fivehundred
 
 type BidSort() =
+
+    member this.SortSuit(bid1:Bid, bid2:Bid) =
+        let order = [ Hearts; Diamonds; Clubs; Spades]
+        match bid1, bid2 with
+        | Bid(_, suit1), Bid(_, suit2) ->
+            let suitIndex1 = order |> Seq.findIndex (fun suit -> suit = suit1)
+            let suitIndex2 = order |> Seq.findIndex (fun suit -> suit = suit2)
+            suitIndex1-suitIndex2
+        | _ -> 0
     
     member this.Sort(bid1:Bid, bid2:Bid) =
         match bid1, bid2 with
-        | Bid(value1, _), Bid(value2, _) -> value2-value1
+        | Bid(value1, _), Bid(value2, _) -> 
+            if (value1 = value2) then this.SortSuit(bid1, bid2)
+            else value2-value1 
         | (NoTrumps(value1), Bid(value2, _)) 
         | (Bid(value2, _), NoTrumps(value1)) -> 
             if (value1 < value2) then 1 else
             if (value1 = value2) then 1 else 
             0
-        | NoTrumps(value1), NoTrumps(value2) -> value1-value2
-        | _ -> 0
+        | NoTrumps(value1), NoTrumps(value2) -> 
+            value2-value1
+        | _ -> 
+            0
         
 
 type Bidding(playersCards) =
